04b5d14418bae1c9c507c91ac39453d8c037713a
sangrammohapatra <sangrammohapatra0203@gmail.com>
2023-06-16 15:20:21 +0530

    16th June Commit 2

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,10 +1,21 @@
 // import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-import Foodmenu from "./Foodmenu.json";
+import Foodmenu from "../../assets/Foodmenu.json";
 import { Card } from "@mui/material";
+// import { useEffect } from "react";
 
 const AvailableMeals = () => {
+  // useEffect(() => {
+  //   const getMeal = async () => {
+  //     const response = await fetch(
+  //       "https://49af6d73-8488-4cfb-8edb-2938f19f3f56.mock.pstmn.io/getMeal"
+  //     );
+  //     const data = await response.json();
+  //     console.log(data);
+  //   };
+  //   getMeal();
+  // }, []);
   const mealsList = Foodmenu.foods.map((meal) => (
     <MealItem
       id={meal.id}
